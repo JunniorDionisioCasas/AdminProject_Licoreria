4c4428703014ac9cfd30817a3d1a2398d1b38dd2
JunniorDionisioCasas <i1720115@continental.edu.pe>
2023-01-19 03:20:27 -0500

    Update home view

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -7,7 +7,22 @@
 @stop
 
 @section('content')
-    <p>Bienvenido</p>
+    <p>Bienvenido {{\Auth::user()->name}}</p>
+    @php
+        echo(Carbon\Carbon::now('-05:00')->isoFormat('d'));
+        $weekDays = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+        $today = Carbon\Carbon::now('-05:00')->isoFormat('d');
+        $barChartsLabels = [];
+        for($i=0; $i<7; $i++){
+            $weekIndex = $today + $i + 1;
+            if($weekIndex>6) $weekIndex = $weekIndex - 7;
+            $barChartsLabels[$i] = $weekDays[$weekIndex];
+        }
+        print_r($barChartsLabels);
+    @endphp
+    <div>
+        <canvas id="myChart" aria-label="Gráfico de ventas" role="img"></canvas>
+    </div>
 @stop
 
 @section('css')
@@ -15,5 +30,52 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="js/urlDomain.js"></script>
+    <script>
+        const url = urlDominio+'api/admin_home_info';
+
+        //llamado al api provincias, index
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+        })
+        .catch(error => console.log(error));
+
+        const ctx = document.getElementById('myChart');
+        /* const data = [
+            { year: 2010, count: 10 },
+            { year: 2011, count: 20 },
+            { year: 2012, count: 15 },
+            { year: 2013, count: 25 },
+            { year: 2014, count: 22 },
+            { year: 2015, count: 30 },
+            { year: 2016, count: 28 },
+        ];
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: data.map(row => row.year),
+                datasets: [{
+                    label: 'Acquisitions by year',
+                    data: data.map(row => row.count),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+            }
+        }); */
+    </script>
 @stop
